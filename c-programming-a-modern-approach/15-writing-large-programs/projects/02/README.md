1cf3fe2277b9ed60771b43ad021d28c3f46b0505
thomcleary <thomasclearydev@gmail.com>
2024-09-25 17:06:08 +0800

    CPaMA: Chapter 15 - Projects (2)

---
+++ c-programming-a-modern-approach/15-writing-large-programs/projects/02/README.md
@@ -0,0 +1,3 @@
+# Project 2
+
+Modify the `justify` example by having the `read_word` function (instead of `main`) store the `*` character at the end of a word that's been truncated.
---
+++ c-programming-a-modern-approach/15-writing-large-programs/projects/02/word.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "word.h"
+
+int read_char(void)
+{
+    int ch = getchar();
+
+    if (ch == '\n' || ch == '\t')
+    {
+        return ' ';
+    }
+    return ch;
+}
+
+int read_word(char *word, int len)
+{
+    int ch, pos = 0;
+
+    while ((ch = read_char()) == ' ')
+    {
+        ;
+    }
+
+    while (ch != ' ' && ch != EOF)
+    {
+        if (pos < len)
+        {
+            word[pos] = pos == (len - 1) ? '*' : ch;
+            pos++;
+        }
+        ch = read_char();
+    }
+    word[pos] = '\0';
+
+    return pos;
+}
