86ef702846e00fd60d70503f3b2b29be4b08b554
thomcleary <thomasclearydev@gmail.com>
2024-10-11 16:59:09 +0800

    WaHT: Part 5 - Methods

--- write-a-hash-table/src/hash_table.c
+++ write-a-hash-table/src/hash_table.c
@@ -8,6 +8,13 @@
 #define HT_PRIME_1 137
 #define HT_PRIME_2 263
 
+// Deleting from an open addressed hash table is more complicated than inserting or searching.
+// The item we wish to delete may be part of a collision chain.
+// Removing it from the table will break that chain, and will make finding items in the tail of the chain impossible.
+// To solve this, instead of deleting the item, we simply mark it as deleted.
+// We mark an item as deleted by replacing it with a pointer to a global sentinel item which represents that a bucket contains a deleted item.
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
 // NOTE: Tutorial skips NULL checks for allocations
 
 static ht_item *ht_new_item(const char *k, const char *v)
@@ -34,15 +41,25 @@ static int ht_hash(const char *s, const int a, const int m)
     for (int i = 0; i < len_s; i++)
     {
         hash += (long)pow(a, len_s - (i + 1)) * s[i];
-        hash = hash % m;
     }
+    hash = hash % m;
     return (int)(hash);
 }
 
 static int ht_get_hash(const char *s, const int num_buckets, const int attempt)
 {
     const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
-    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+
+    // If hash_b == (num_buckets - 1),
+    // then (hash_b + 1) will result in the return statement being equivalent to
+    // hash_a % num_buckets
+    // and can cause infinite cycles when probing the hash table
+    if (hash_b % num_buckets == 0)
+    {
+        hash_b = 1;
+    }
+
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
 
@@ -69,4 +86,79 @@ void ht_del_hash_table(ht_hash_table *ht)
     }
     free(ht->items);
     free(ht);
+}
+
+void ht_insert(ht_hash_table *ht, const char *key, const char *value)
+{
+    ht_item *item = ht_new_item(key, value);
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item *cur_item = ht->items[index];
+
+    int i = 1;
+    while (cur_item != NULL)
+    {
+        // This looks like it will leave deleted items in the array forever?
+        if (cur_item != &HT_DELETED_ITEM)
+        {
+            if (strcmp(cur_item->key, key) == 0)
+            {
+                ht_del_item(cur_item);
+                ht->items[index] = item;
+                return;
+            }
+        }
+        index = ht_get_hash(item->key, ht->size, i);
+        cur_item = ht->items[index];
+        i++;
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
+
+char *ht_search(ht_hash_table *ht, const char *key)
+{
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item *item = ht->items[index];
+
+    int i = 1;
+    while (item != NULL)
+    {
+        if (item != &HT_DELETED_ITEM)
+        {
+            if (strcmp(item->key, key) == 0)
+            {
+                return item->value;
+            }
+        }
+
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+
+    return NULL;
+}
+
+void ht_delete(ht_hash_table *ht, const char *key)
+{
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item *item = ht->items[index];
+
+    int i = 1;
+    while (item != NULL)
+    {
+        if (item != &HT_DELETED_ITEM)
+        {
+            if (strcmp(item->key, key) == 0)
+            {
+                ht_del_item(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+                ht->count--;
+                return;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
 }
--- write-a-hash-table/src/hash_table.h
+++ write-a-hash-table/src/hash_table.h
@@ -16,5 +16,8 @@ typedef struct
 
 ht_hash_table *ht_new(void);
 void ht_del_hash_table(ht_hash_table *ht);
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
+char *ht_search(ht_hash_table *ht, const char *key);
+void ht_delete(ht_hash_table *ht, const char *key);
 
 #endif
